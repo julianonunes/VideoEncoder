91f8e88ce483329d23f0d2520a3e79b265a1b0a0
Oliveira Juliano <ju.oliveira@reply.it>
2015-11-03 01:42:46 -0200

    Código de upload para Amazon S3 finalizado e teste obtendo sinal verde

--- src/main/java/videoencoder/storage/S3Uploader.java
+++ src/main/java/videoencoder/storage/S3Uploader.java
@@ -1,15 +1,57 @@
 package videoencoder.storage;
 
 import java.io.File;
+import java.util.UUID;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 
 import videoencoder.storage.base.IUploader;
 
 public class S3Uploader implements IUploader {
 
+	private static final String BUCKET_NAME = "videoencoder-objects";	
+	
 	@Override
 	public boolean sendFile(File file) {
-		// TODO Auto-generated method stub
+		AmazonS3 s3Client = new AmazonS3Client(new ProfileCredentialsProvider());
+		
+		try {
+			// Atribuo um UUID aleatório ao enviar para o Amazon S3.
+			PutObjectResult result = s3Client.putObject(
+					new PutObjectRequest(BUCKET_NAME, UUID.randomUUID().toString(), file));
+		
+			// Se diferente de nulo, obteve sucesso (true).
+			return (result != null);
+		
+		}
+		catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " +
+            		"means your request made it " +
+                    "to Amazon S3, but was rejected with an error response" +
+                    " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        } 
+		catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " +
+            		"means the client encountered " +
+                    "an internal error while trying to " +
+                    "communicate with S3, " +
+                    "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+        }
+		
 		return false;
+		
 	}
 
 }
--- src/test/java/videoencoder/storage/UploaderTest.java
+++ src/test/java/videoencoder/storage/UploaderTest.java
@@ -2,17 +2,46 @@ package videoencoder.storage;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
 import org.junit.Test;
 
 import videoencoder.storage.base.IUploader;
 
 public class UploaderTest {
 
+	/**
+     * Creates a temporary file with text data to demonstrate uploading a file
+     * to Amazon S3
+     *
+     * @return A newly created temporary file with text data.
+     *
+     * @throws IOException
+     */
+    private static File createSampleFile() throws IOException {
+        File file = File.createTempFile("aws-java-sdk-", ".txt");
+        file.deleteOnExit();
+
+        Writer writer = new OutputStreamWriter(new FileOutputStream(file));
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("!@#$%^&*()-=[]{};':',.<>/?\n");
+        writer.write("01234567890112345678901234\n");
+        writer.write("abcdefghijklmnopqrstuvwxyz\n");
+        writer.close();
+
+        return file;
+    }
+	
 	@Test
-	public void mustReturnTrueForSuccessfulUpload() {
+	public void mustReturnTrueForSuccessfulUpload() throws IOException {
 		IUploader uploader = new S3Uploader();
-		
-		assertTrue(uploader.sendFile(null));
+			
+		assertTrue(uploader.sendFile(createSampleFile()));
 	}
 
 }
