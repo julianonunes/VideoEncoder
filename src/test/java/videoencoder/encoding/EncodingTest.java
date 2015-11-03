4949a4b0dc80886c8874c0dd018cbf6260d76480
Oliveira Juliano <ju.oliveira@reply.it>
2015-11-02 23:56:19 -0200

    Criação do código base para conversão, criação da classe de teste e mudança na estrutura de pastas

---
+++ src/test/java/videoencoder/encoding/EncodingTest.java
@@ -0,0 +1,14 @@
+package videoencoder.encoding;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncodingTest {
+
+	@Test
+	public void testEncode() {
+		fail("Not yet implemented");
+	}
+
+}
