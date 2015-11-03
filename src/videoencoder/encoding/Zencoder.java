9756f069e941cb7538f2e08f4839647da46b41bc
Oliveira Juliano <ju.oliveira@reply.it>
2015-11-02 23:27:36 -0200

    Criação do helper de conversão

---
+++ src/videoencoder/encoding/Zencoder.java
@@ -0,0 +1,15 @@
+package videoencoder.encoding;
+
+import videoencoder.encoding.base.IEncoder;
+
+public class Zencoder implements IEncoder {
+
+	@Override
+	public void encode(String fileUrl) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ src/videoencoder/encoding/base/IEncoder.java
@@ -0,0 +1,7 @@
+package videoencoder.encoding.base;
+
+public interface IEncoder {
+	
+	public void encode(String fileUrl);
+
+}
